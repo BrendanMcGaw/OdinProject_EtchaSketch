f7fddc7e61565c6b6e0c07c6993fae15f829ae69
Brendan McGaw <Brendanmcgaw@gmail.com>
2023-09-26 22:52:14 +1000

    Added button, added button functionality, allow user input prompted for amount of squares. Added numbers to be calculated to find the grid size, modified functionality to create grid based on the total squares featured. Added functionality to find the percentage of width and height required in order to fill the space with squares. Changed those percentages toStrings in order to use them as numerical string inputs for CSS changes.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,36 @@
 
-let  targetContainer = document.querySelector(".sketch-container");
+let targetContainer = document.querySelector(".sketch-container");
+// getElementsByClassName actually returns an array. So you must select 0 to target the box as its in position 0 of the array.
+let promptButton = document.getElementsByClassName("promptButton")[0];
+let totalSquares;
+let numberOfSquares = 0;  // = prompt("Please enter the amount of squares you wish to use.");
+let sizePercentage;
+
+promptButton.addEventListener("click", function() {
+    numberOfSquares = prompt("Please enter an amount of squares, less than 100.");
+    if (numberOfSquares > 100) {
+        alert("You have entered too high a count. Please try again.")
+        numberOfSquares = prompt("Please enter an amount of squares, less than 100.");
+    }
+    calculateBoxSize();
+    totalSquares = numberOfSquares *  numberOfSquares;
+    gridBuilder();
+});
+
+
+calculateBoxSize = () => {
+    sizePercentage = 100 / numberOfSquares;
+}
+// We want  to allow the user to input a numberto use  in the grid.
+// We therefore must allow the boxbuilder box width  and heig2133ht to be calculated based on a mathematical formula used from the amount of boxes per side.
+// 100 /  (User input number.) == Percentage both width and height.
+// %width %height = 100 / (userInputNumber)55
+
+
+
 
 gridBuilder = () => {
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < totalSquares; i++) {
         boxBuilder();
     }
 }
@@ -11,8 +39,8 @@ boxBuilder = () => {
     const box = document.createElement("div");
     box.classList.add('box');
     box.style.display = "flex";
-    box.style.width = "25px";
-    box.style.height = "25px";
+    box.style.width = sizePercentage.toString() + "%";
+    box.style.height = sizePercentage.toString() + "%";
     box.style.background = "white";
     targetContainer.appendChild(box);
     box.addEventListener("mouseover", function() {
